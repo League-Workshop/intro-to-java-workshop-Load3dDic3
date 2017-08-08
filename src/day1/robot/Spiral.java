c6175f89b87e7348c57447e6237d118a28823f0e
ArnavPalkhiwala <league@WTS20.attlocal.net>
2017-08-08 11:52:20 -0700

    Shapes

--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -6,22 +6,29 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot j = new Robot();
+
 		// 5. Set your robot's pen to the down position
-
+		j.penDown();
 		// 3. Set the robot to go at max speed (10)
-
+		j.setSpeed(10);
 		// 4. Do the following (steps 6-9) 75 times
+		for (int i = 0; i < 10000; i++) {
 
 			// 7. Change the pen color to random
-	
-			// 6. Move the robot 5 times the current line number you are drawing (5*i)
-	
+			j.setRandomPenColor();
+			// 6. Move the robot 5 times the current line number you are drawing
+			// (5*i)
+			j.move(3 * i);
 			// 2. Turn the robot 1/3 of 360 degrees to the right
-	
-			// 8. Change the number of sides to 7 (don’t add a new line of code for this one!)
-	
-			// 9. Set the pen width to i
+			j.turn(360 / 3);
 
+			// 8. Change the number of sides to 7 (don’t add a new line of code
+			// for
+			j.setPenWidth(5);
+			// this one!)
+
+			// 9. Set the pen width to i
+		}
 	}
 }
--- src/day1/robot/TriangleShell.java
+++ src/day1/robot/TriangleShell.java
@@ -5,35 +5,45 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
+	Robot j = new Robot();
 
-	
 	void go() {
-		drawTriangle(100); // 3. delete this line (used only for testing)
+		// 3. delete this line (used only for testing)
 
 		// 6. Make the robot go as fast as possible
-
-		// 4. make a variable to hold the length of the triangle and set it to 50
-
+		j.setSpeed(10);
+		// 4. make a variable to hold the length of the triangle and set it to
+		// 50
+		int length = 50;
 		// 7. Do the following (up to step 10) 60 times
+		for (int i = 0; i < 60; i++) {
 
 			// 9. Change the color of the pen to a random color
-	
+			j.setRandomPenColor();
 			// 8. Increase the length of the side by 10 pixels
-	
+			length = length + 10;
 			// 5. call your drawTriangle() method using your length variable
-	
+			drawTriangle(length);
 			// 10. Turn the tortoise 6 degrees to the right
-
+			j.turn(6);
+			j.setPenWidth(15);
+		}
 	}
 
-	/* 2. fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
+	/*
+	 * 2. fill in the method below to draw a triangle. Use the length variable
+	 * for the size of the triangle.
+	 */
 	private void drawTriangle(int length) {
-		
-		
-		
+		j.penDown();
+		j.setSpeed(10);
+		for (int i = 0; i < 3; i++) {
+
+			j.move(length);
+			j.turn(360 / 3);
+		}
 	}
 
-	
 	public static void main(String[] args) {
 		new TriangleShell().go();
 	}
